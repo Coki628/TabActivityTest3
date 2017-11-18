123c5c2923465ec70e48db66fcff64aa7a0c231c
MYCOMPUTER\srcn4 <srcn4frdm.c@gmail.com>
2017-11-18 23:42:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/srcn4/tabactivitytest3/PageFragment.java
@@ -0,0 +1,78 @@
+package com.example.srcn4.tabactivitytest3;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class PageFragment extends Fragment {
+
+    private static final String ARG_PARAM = "page";
+    private static final String ARG_PARAM_STR = "page_str";
+    private String mParam;
+    private OnFragmentInteractionListener mListener;
+
+    // コンストラクタ
+    public PageFragment() {
+    }
+
+    public static PageFragment newInstance(int page) {
+        PageFragment fragment = new PageFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PARAM, page);
+        args.putString(ARG_PARAM_STR, String.valueOf(page));
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam = getArguments().getString(ARG_PARAM_STR);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        int page = getArguments().getInt(ARG_PARAM, 0);
+        View view = inflater.inflate(R.layout.fragment_page, container, false);
+        TextView textView = view.findViewById(R.id.textView);
+        textView.setText("Page" + page);
+
+        return view;
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
